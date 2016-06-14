2b1ec15303efe453847d6286ce43f11565697eec
Eduardo Moreno <eduardocmoreno@gmail.com>
2016-06-14 10:32:37 -0300

    edbox page

---
+++ edbox/php/extpage.php
@@ -0,0 +1,20 @@
+<style>
+	#target4{
+		background: #fff;
+		color: #666;
+		padding:40px;
+		text-align:justify;
+	}
+	#target4 h2{
+		margin: 0 0 20px;
+	}
+	#target4 h2,
+	#target4 .exPageLink{
+		color: #333;
+	}
+</style>
+<div id="target4">
+	<h2>Hey! That's the example 4!</h2>
+	<p>Parameter: <?php echo $_GET['parameter']; ?></p>
+	<a class="exPagelink" href="#" onclick="$.edbox.close(); return false;">close the box!</a>
+</div>
